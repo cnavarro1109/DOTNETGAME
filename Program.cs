bbff827c03c8d7bc14c1761a01435cf1e466271e
Rob Galvan <robgalvan@gmail.com>
2017-06-13 18:50:30 -0500

    added a new alien and added unique messages.

--- Program.cs
+++ Program.cs
@@ -12,9 +12,13 @@ namespace Game
             Console.WriteLine("As you walk into the kitchen the light go out !");
             Console.WriteLine("Then you hear a hissing sound coming from the refrigerator.");
 
+
+            //create hero here
             Hero hero = new Hero();
 
-            Console.WriteLine("What is your name human!");
+
+            Console.WriteLine("You are so shocked you can't even remember your own name...");
+            Console.WriteLine("What is my name again  ??? ... Remember and type your name... if you want to live.");
             hero.heroName = Console.ReadLine();
 
             Console.WriteLine("Think quick what are you going to do {0}???", hero.heroName);
@@ -36,6 +40,14 @@ namespace Game
             string[,] weapons1 = new string[3, 2] { { "Torch (it)", "1000" }, { "Freeze (make harder)", "100" }, { "Candle (burn with prayers)", "500" } };
             string[,] weapons2 = new string[3, 2] { { "Spoon (her)", "1000" }, { "Whip Cream (on it)", "100" }, { "Fork (it)", "500" } };
             string[,] weapons3 = new string[3, 2] { { "Toothpick (poker face)", "1000" }, { "Vodka (martini)", "100" }, { "Mouth (smash)", "500" } };
+            string[,] weapons4 = new string[3, 2] { { "Banana (hes slips)", "1000" }, { "Tha Look (makes him rowdy", "100" }, { "Banana (hes slips)" ,"500" } };
+
+            //
+            // string message1 = "You have melted your friend and you left behind a puddle.";
+
+            //  , {"you made her love you so good that she die from broke heart"},{"you kill him"},{""}};
+            // //
+
 
 
             Alien alien1 = new Alien();
@@ -43,43 +55,46 @@ namespace Game
             alien1.alienName = "Patrick";
             alien1.strenght = new string[1] { "Freeze (make harder)" };
             alien1.weakness = new string[2] { "Torch (it)", "Candle (burn with prayers)" };
+            alien1.message =  "You have melted your friend and you left behind a puddle.";
+
 
             Alien alien2 = new Alien();
             alien2.healthPoints = 1000;
             alien2.alienName = "Anna";
             alien2.strenght = new string[1] { "Whip Cream (on it)" };
             alien2.weakness = new string[2] { "Spoon (her)", "Fork (it)" };
+            alien2.message = "You made her love you so good that she died from broke heart";
 
             Alien alien3 = new Alien();
             alien3.healthPoints = 1000;
             alien3.alienName = "Gaga";
             alien3.strenght = new string[1] { "Vodka (martini)" };
             alien3.weakness = new string[2] { "Toothpick (poker face)", "Mouth (smash)" };
+            alien3.message = "You rowdy hurt her and now shes 86'd";
+            // new alien in the game
+            Alien alien4 = new Alien();
+            alien4.healthPoints = 1000;
+            alien4.alienName = "Da Baws";
+            alien4.strenght = new string[1] { "Tha Look (makes him rowdy)" };
+            alien4.weakness = new string[2] { "Banana (hes slips)", "slap (feels disrespected)" };
+            alien4.message = "You are now safe... good job me.";
 
 
 
-
-            var stages = new List<Tuple<Alien, Hero, string[,],string>>();
-            stages.Add(Tuple.Create(alien1, hero, weapons1,alienDraw1()));
-            stages.Add(Tuple.Create(alien2, hero, weapons2,alienDraw2()));
-            stages.Add(Tuple.Create(alien3, hero, weapons3,alienDraw3()));
+            var stages = new List<Tuple<Alien, Hero, string[,],string,string>>();
+            stages.Add(Tuple.Create(alien1, hero, weapons1,alienDraw1(),alien1.message));
+            stages.Add(Tuple.Create(alien2, hero, weapons2,alienDraw2(),alien2.message));
+            stages.Add(Tuple.Create(alien3, hero, weapons3,alienDraw3(),alien3.message));
+            stages.Add(Tuple.Create(alien4, hero, weapons4,alienDraw4(),alien4.message));
 
             foreach (var stage in stages)
             {
-
-                //Moved to method stageCompleted
+             //Moved to method stageCompleted
                 stageCompleted(stage);
             }
-
-
-
-
-
-           
-
         }
 
-        public static bool stageCompleted(Tuple<Alien, Hero, string[,],string> stage)
+        public static bool stageCompleted(Tuple<Alien, Hero, string[,],string,string> stage)
         {
 
             //Define your variables
@@ -87,6 +102,7 @@ namespace Game
             var hero = stage.Item2;
             var weapons1 = stage.Item3;
             var enemyDraw = stage.Item4;
+            var message = stage.Item5;
 
             while (alien1.healthPoints > 0 && hero.lives > 0)
             {
@@ -126,26 +142,21 @@ namespace Game
                 }
             }
 
-
-
             if (alien1.healthPoints <= 0)
             {
-                Console.WriteLine("You have melted your friend and you left behind a puddle.");
-                Console.WriteLine("You managed to defeat the foe with {0} lives remaining... Good Luck {1}", hero.lives, hero.heroName);
+                Console.WriteLine("-++----===+===----==+==----===+===----++-");
+                
+                Console.WriteLine("You managed to defeat -[ {0} ]- with {1} lives remaining.", alien1.alienName , hero.lives);
+                Console.WriteLine("{0}",alien1.message);
                 return true;
             }
             else if (hero.lives <= 0)
             {
-                Console.WriteLine("You have expended your stay on this earth my friend... goodbye.");
+                Console.WriteLine("You have expended your stay on this earth... goodbye.");
                 return false;
             }
-
-
             return false;
         }
-
-
-
         public static string alienDraw1()
         {
             return @"
@@ -188,6 +199,24 @@ namespace Game
 \    -===-==ANNA==-====- .'=.|./
  '.         .''.           .'
    '-.....-'   '-........-'
+";
+            
+        }
+        public static string alienDraw4()
+        {
+            return @"	
+		
+    _   _   _   _
+   / \_/ \_/ \ / | /  ___
+   +++  ++    \   /  )<i,]=---
+   |  | |  ( o ) ( o )   ]
+   | / /    \ /   \ / =  ] ---o
+   |/ /      ) . __. .___.,=-
+   \ / <>     (  __  )    .\\\
+    .....vv    \/ \/      /
+    :';:''' '' '' '' '' ''..
+    ===w==w==\/\/\/\/\/\/.:
+    (_._._._)(_._/._]])
 ";
             
         }
@@ -239,13 +268,12 @@ namespace Game
             get;
             set;
         }
-    }
 
-    // public class Weapons
-    // {
-    //     string[,] weapons1 = new string[3, 2] { { "Spoon (her)", "9000" }, { "Whip Cream (on it)", "1" }, { "Fork (it)", "25" } };
-    //     string[,] weapons2 = new string[3, 2] { { "Torch (it)", "9000" }, { "Freeze (make harder)", "1" }, { "Candle (burn with prayers)", "5" } };
-    //     string[,] weapons3 = new string[3, 2] { { "Toothpick (poker face)", "9000" }, { "Vodka (martini)", "1" }, { "Mouth (smash)", "15" } };
-    // }
+        public string message
+        {
+            get;
+            set;
+        }
+    }
 
 }
